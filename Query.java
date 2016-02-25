a20ca093a55a48f7fe498833b1de96fd0fb06c13
Brian Cechmanek <bcechmanek@gmail.com>
2016-02-24 21:26:13 -0700

    separated Query to start initial search algo

---
+++ Query.java
@@ -0,0 +1,13 @@
+package com.cechmanek.taxonomy;
+
+public class Query {
+    protected static String[] query;
+
+    public static void copy(String[] query2input) {
+        query = query2input;
+	}   
+    
+    private void binSearch(String[] query) {
+    	// TODO devise initial search algo
+    }
+}
